c3101782a7591267e1b949f5994b4776d90c294c
sathy <sathyaramece@gmail.com>
2024-06-19 23:23:40 +0530

    Combine 3 merge into 1

--- SimplestCRUDExample/src/main/java/com/example/SimplestCRUDExample/SimplestCrudExampleApplication.java
+++ SimplestCRUDExample/src/main/java/com/example/SimplestCRUDExample/SimplestCrudExampleApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SimplestCrudExampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SimplestCrudExampleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SimplestCrudExampleApplication.class, args);
+        System.out.println("Change 1"0);
+        System.out.println("Change 2"0);
+        System.out.println("Change 3"0);
+    }
 
 }
